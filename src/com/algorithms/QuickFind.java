05214a92d7dc6f16a0617bdd02cac99a9aa95a95
ringo <ldurazo@nearsoft.com>
2014-09-16 14:25:47 -0700

    Added quickfind

---
+++ src/com/algorithms/QuickFind.java
@@ -0,0 +1,26 @@
+package com.algorithms;
+
+public class QuickFind {
+    private int[] id;
+
+    public QuickFind(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i]=i;
+        }
+    }
+
+    //1 operation
+    public boolean find(int p, int q) {
+        return id[p]==id[q];
+    }
+
+    //N operations
+    public void unite(int p, int q) {
+        int pid=id[p];
+        for (int i = 0; i < id.length; i++) {
+            if(id[i]==pid) id[i]=id[q];
+        }
+    }
+
+}
